0b33ccce759292b513d775ef09aaceb252ad5a62
Irina Bogdanova <irinabogdanova261@gmail.com>
2024-07-17 06:47:04 +0000

    Add a description of the app at the system start

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from googlesheets import GoogleSheetsClient
 from authors import Authors
 from books import Books
 from menu import Menu
+from colorama import Fore
 
 
 CREDS = GoogleSheetsClient.get_creds("creds.json")
@@ -21,7 +22,15 @@ def main():
     books_manager = Books(client.open_worksheet(sheet_name, "books"))
 
     # Create a Menu instance
-    print("Welcome to Library Data Automation")
+    print(
+        Fore.BLUE +
+        f"Welcome to Library Data Automation!\n"
+        f"This application stores information about authors and books. "
+        f"It allows the user to get the list of authors and books, "
+        f"add a new author and a new book, edit existing authors and books "
+        f"and display books with a section by an author and a part of "
+        f"the title."
+    )
     menu = Menu(authors_manager, books_manager)
 
     # Display the main menu
